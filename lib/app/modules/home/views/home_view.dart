e498cf1e5d1f59664673b1ebfd6322da0d28fa5e
thanchayawik <thanchayawik@gmail.com>
2024-09-21 22:36:59 +0700

    add your goal

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:habit_frontend/app/layout/bottom_app_bar.dart';
 import 'package:habit_frontend/app/modules/home/widgets/progress_card.dart';
 import 'package:habit_frontend/app/modules/home/widgets/today_habit.dart';
+import 'package:habit_frontend/app/modules/home/widgets/your_goals.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -12,35 +13,39 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomAppBarWidget(),
-      body: Container(
-        color: Colors.grey[100],
-        padding: const EdgeInsets.only(left: 12.0, right: 12.0, top: 64.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
+        bottomNavigationBar: BottomAppBarWidget(),
+        body: SingleChildScrollView(
+          child: Container(
+            color: Colors.grey[100],
+            padding: const EdgeInsets.only(left: 12.0, right: 12.0, top: 64.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  'Hello,',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                Row(
+                  children: [
+                    Text(
+                      'Hello,',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Than',
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.deepOrange[500]),
+                    )
+                  ],
                 ),
-                Text(
-                  'Than',
-                  style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.deepOrange[500]),
-                )
+                SizedBox(height: 16),
+                ProgressCard(),
+                SizedBox(height: 10),
+                TodayHabit(),
+                SizedBox(height: 10),
+                YourGoals()
               ],
             ),
-            SizedBox(height: 16),
-            ProgressCard(),
-            SizedBox(height: 10),
-            TodayHabit()
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
--- lib/app/modules/home/widgets/your_goals.dart
+++ lib/app/modules/home/widgets/your_goals.dart
@@ -1,10 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:habit_frontend/app/data/models/habit.dart';
 
 class YourGoals extends StatelessWidget {
   const YourGoals({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    List<Habit> todayHabits = [
+      Habit(title: "Drink Water", description: "Drink Water 8 cups"),
+      Habit(title: "Exercise", description: "30 minutes of workout"),
+      Habit(title: "Read", description: "Read for 20 minutes"),
+    ];
+
+    return Card(
+      color: Colors.white,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 450,
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Your Goals',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                Text(
+                  'See all',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      color: Colors.deepOrange[500]),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: todayHabits.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                      margin: EdgeInsets.only(bottom: 8), // Space between items
+                      decoration: BoxDecoration(
+                        color: Colors.grey[100], // Grey background
+                        borderRadius:
+                            BorderRadius.circular(12), // Rounded corners
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  todayHabits[index].title,
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 16),
+                                ),
+                                Icon(Icons.more_vert)
+                              ],
+                            ),
+                            SizedBox(height: 8),
+                            LinearProgressIndicator(
+                              value: 0.5,
+                              backgroundColor: Colors.grey[300],
+                              valueColor: const AlwaysStoppedAnimation<Color>(
+                                  Colors.deepPurple),
+                              minHeight: 10,
+                              borderRadius: BorderRadius.circular(4),
+                              // Adjust thickness as needed
+                            ),
+                            SizedBox(height: 8),
+                            Text("5 fron 7 day target"),
+                            Text(
+                              "Everyday",
+                              style: TextStyle(color: Colors.deepOrange[500]),
+                            ),
+                          ],
+                        ),
+                      ));
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
