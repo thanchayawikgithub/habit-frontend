52e62d5a3eb3e678d48d2d8f4b640c54a6d1751a
jettada <jettada.kongchai@gmail.com>
2024-10-08 23:53:57 +0700

    update

--- lib/app/modules/habits/views/habit_detail.dart
+++ lib/app/modules/habits/views/habit_detail.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:habit_frontend/app/data/models/habit.dart';
-import 'package:habit_frontend/app/data/models/habit_record.dart';
 import 'package:habit_frontend/app/modules/habits/controllers/habits_controller.dart';
 
 import 'package:habit_frontend/app/modules/habits/controllers/habits_detail_controller.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-import '../widget/calender.dart';
-
-// ignore: must_be_immutable
 class HabitDetailView extends GetView<HabitDetailController> {
   HabitsController habitsCtrl = Get.put(HabitsController());
   String? habitId = Get.parameters['id'];
@@ -29,84 +25,17 @@ class HabitDetailView extends GetView<HabitDetailController> {
         String status = record.status == 'Complete' || record.status == true
             ? 'Complete'
             : 'Pending';
-        habitStatusMap[date] = status; // เก็บสถานะใน Map
+        habitStatusMap[date] = status;
       }
 
       return Scaffold(
-<<<<<<< HEAD
-          appBar: AppBar(
-              backgroundColor: Colors.white,
-              leading: IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () {
-                  Get.back(); // กลับไปยังหน้าก่อนหน้า
-                },
-              ),
-              title: Text(habit.title,
-                  style: const TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ))),
-          body: Container(
-            width: double.infinity,
-            color: Colors.white,
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    const Text(
-                      'Description: ',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                    ),
-                    Text(
-                      habit.description,
-                      style: const TextStyle(fontSize: 20),
-                    ),
-                    Text(
-                      habit.description,
-                      style: const TextStyle(fontSize: 20),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    const Text(
-                      'Icon: ',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                    ),
-                    Icon(habit.icon),
-                  ],
-                ),
-                const Text(
-                  'Records',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                ),
-                Expanded(
-                    child: ListView.builder(
-                  itemCount: habit.habitRecords.length,
-                  itemBuilder: (context, index) {
-                    HabitRecord habitRecord = habit.habitRecords[index];
-                    return ListTile(
-                      title: Text('Date : ${habitRecord.date}'),
-                      trailing: Text(
-                          'Status ${habitRecord.status == true ? 'Complete' : 'Pending'}'),
-                    );
-                  },
-                ))
-              ],
-=======
         appBar: AppBar(
             backgroundColor: Colors.white,
             leading: IconButton(
               icon: const Icon(Icons.arrow_back),
               onPressed: () {
-                Get.back(); // กลับไปยังหน้าก่อนหน้า
+                Get.back();
               },
->>>>>>> a330fbe3468b43603faafd5a14f5231e756301a9
             ),
             title: Text(habit.title,
                 style: const TextStyle(
@@ -126,8 +55,8 @@ class HabitDetailView extends GetView<HabitDetailController> {
                 firstDay: DateTime(2010, 10, 16),
                 lastDay: DateTime(2030, 3, 14),
                 headerStyle: HeaderStyle(
-                  formatButtonVisible: false, // ซ่อนปุ่ม "2 weeks"
-                  titleCentered: true, // จัดกลางหัวข้อ
+                  formatButtonVisible: false,
+                  titleCentered: true,
                 ),
                 calendarBuilders: CalendarBuilders(
                   markerBuilder: (context, date, events) {
@@ -178,10 +107,10 @@ class HabitDetailView extends GetView<HabitDetailController> {
                 Padding(
                   padding: EdgeInsets.all(6),
                   child: Container(
-                    padding: EdgeInsets.all(6), // ระยะห่างภายใน
+                    padding: EdgeInsets.all(6),
                     decoration: BoxDecoration(
-                      color: Colors.green, // สีพื้นหลัง
-                      shape: BoxShape.circle, // รูปทรงกลม
+                      color: Colors.green,
+                      shape: BoxShape.circle,
                     ),
                   ),
                 ),
@@ -195,10 +124,10 @@ class HabitDetailView extends GetView<HabitDetailController> {
                 Padding(
                   padding: EdgeInsets.all(6),
                   child: Container(
-                    padding: EdgeInsets.all(6), // ระยะห่างภายใน
+                    padding: EdgeInsets.all(6),
                     decoration: BoxDecoration(
-                      color: Colors.red, // สีพื้นหลัง
-                      shape: BoxShape.circle, // รูปทรงกลม
+                      color: Colors.red,
+                      shape: BoxShape.circle,
                     ),
                   ),
                 ),
@@ -212,11 +141,10 @@ class HabitDetailView extends GetView<HabitDetailController> {
                 Padding(
                   padding: EdgeInsets.all(6),
                   child: Container(
-                    padding: EdgeInsets.all(6), // ระยะห่างภายใน
+                    padding: EdgeInsets.all(6),
                     decoration: BoxDecoration(
-                      color: const Color.fromARGB(
-                          255, 183, 171, 248), // สีพื้นหลัง
-                      shape: BoxShape.circle, // รูปทรงกลม
+                      color: const Color.fromARGB(255, 183, 171, 248),
+                      shape: BoxShape.circle,
                     ),
                   ),
                 ),
